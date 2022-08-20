e40671b73239a9c26c253912c5615792957e04ac
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-08-20 12:58:23 -0700

    Skip the first argument (the command itself)

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@
 use redis_module::{Context, RedisError, RedisResult, RedisString, NextArg};
 
 fn fsm_create(_: &Context, args: Vec<RedisString>) -> RedisResult {
+  let args = args.into_iter().skip(1);
 }
