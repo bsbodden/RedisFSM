870980c40c3d2626b88006a7eb4ea558b4bd32d8
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-08-20 12:57:50 -0700

    Create function for the FSM.CREATE command

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 #[macro_use] extern crate redis_module;
 
 use redis_module::{Context, RedisError, RedisResult, RedisString, NextArg};
+
+fn fsm_create(_: &Context, args: Vec<RedisString>) -> RedisResult {
+}
