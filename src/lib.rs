f1fca7c37e2a0516dd2ec7201ff2a6b1d011e3f2
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-08-20 12:59:26 -0700

    Check that we're only getting one argument

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,8 @@ fn fsm_create(_: &Context, args: Vec<RedisString>) -> RedisResult {
   let args = args.into_iter().skip(1);
   let args_card = args.len();
 
+  if args_card > 1 {
+    return Err(RedisError::WrongArity);
+  }
+
 }
