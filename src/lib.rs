dd5c59f46f3ed661d315bb9dc613b125ec176aea
Brian Sam-Bodden <bsb@redis.com>
2022-08-24 15:22:48 -0700

    Get rid of the hardcoded JSON

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ use serde::{Deserialize, Serialize};
 use redis_module::native_types::RedisType;
 use redis_module::raw::RedisModuleTypeMethods;
 use std::os::raw::c_void;
-use serde_json::json;
 
 //////////////////////////////////////////////////////
 
@@ -61,44 +60,10 @@ pub static REDIS_FSM_TYPE: RedisType = RedisType::new(
 fn fsm_create(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
   let mut args = args.into_iter().skip(1);
   let fsm_json = args.next_arg()?;
+  let fsm: StateMachine = serde_json::from_str(&fsm_json.to_string())?;
+  let key = RedisString::create(ctx.ctx, &fsm.name.to_string());
   let redis_key = ctx.open_key_writable(&key);
 
-  let fsm_json = json!({
-    "name": "JobFSM",
-    "prefix": "job:",
-    "field": "state",
-    "states": [
-      "sleeping",
-      "running",
-      "cleaning"
-    ],
-    "events": [
-      {
-        "name": "run",
-        "from": [
-          "sleeping"
-        ],
-        "to": "running"
-      },
-      {
-        "name": "clean",
-        "from": [
-          "running"
-        ],
-        "to": "cleaning"
-      },
-      {
-        "name": "sleep",
-        "from": [
-          "running",
-          "cleaning"
-        ],
-        "to": "sleeping"
-      }
-    ]
-  });
-
-  let fsm: StateMachine = serde_json::from_str(&fsm_json.to_string())?;
   if let Ok(_) = redis_key.set_value(&REDIS_FSM_TYPE, fsm) {
     return Ok(RedisValue::Integer(true as i64));
   } else {
